70d8f6741687f9921ea786e8969581b3b42bafef
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2020-10-12 12:03:46 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -6,9 +6,8 @@ public:
 	Node* next;
 };
 class LinkedList {
-	
-public:
 	Node* head, * tail;
+public:
 	LinkedList() {
 		head = NULL;
 		tail = NULL;
@@ -16,7 +15,7 @@ public:
 	void insert(int x) {
 		Node* ptr = new Node;
 		ptr->data = x; ptr->next = NULL;
-		if (head == NULL) { 
+		if (head == NULL) {
 			head = ptr; tail = ptr;
 		}
 		else {
@@ -24,58 +23,35 @@ public:
 			tail = ptr;
 		}
 	}
-	void bubbleSort(LinkedList* head) {
-		int swapped;
-		Node* ptr1;
-		Node* lptr = NULL;
-		if (head == NULL)
-			return;
-		do {
-			swapped = 0;
-			ptr1 = this->head;
-			while (ptr1->next != lptr)
-			{
-				if (ptr1->data > ptr1->next->data)
-				{
-					swap(ptr1, ptr1->next);
-					swapped = 1;
-				}
-				ptr1 = ptr1->next;
-			}
-			lptr = ptr1;
-		} while (swapped);
-	}
-	void swap( Node* a,  Node* b) {
-		int temp = a->data;
-		a->data = b->data;
-		b->data = temp;
+	void pairWiseSwap(){
+		Node* temp = head;
+		while (temp != NULL && temp->next != NULL) {
+			swap(temp->data,temp->next->data);
+			temp = temp->next->next;
+		}
 	}
 	void display() {
 		Node* temp;
 		temp = head;
-		cout << "Liast is " << endl;
-		while (temp != NULL) { 
+		cout << "List is " << endl;
+		while (temp != NULL) {
 			cout << temp->data << " ";
 			temp = temp->next;
 		}
-		cout << endl;
 	}
-
 };
 int main() {
-	LinkedList *l1 = new LinkedList;
-	int num;
-	char ch;
-	do {
-		cout << "Enter Number you want to insert "; cin >> num;
-		l1->insert(num);
-		cout << "Enter Y if you want to insert value again "; cin >> ch;
-	} while (ch == 'y' || ch == 'Y');
+	LinkedList* l1 = new LinkedList;
+	l1->insert(5);
+	l1->insert(3);
+	l1->insert(6);
+	l1->insert(1);
 	l1->display();
 	cout << endl;
-	cout << "After sorting ";
-	l1->bubbleSort(l1);
+	l1->pairWiseSwap();
 	l1->display();
+	cout << endl;
 	system("pause");
 	return 0;
+
 }
